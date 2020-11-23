1a154cf491175b20628ccbc8ec8dbfd8fade70a3
苏逸杰 <1299947221@qq.com>
2020-11-23 21:42:26 +0800

    上线版1.0

--- src/com/servlet/BlogServlet.java
+++ src/com/servlet/BlogServlet.java
@@ -56,6 +56,6 @@ public class BlogServlet extends HttpServlet {
         String string = JSON.toJSONString(one);
         PrintWriter printWriter = resp.getWriter();
         printWriter.write(string);
-        System.out.println("master的分支");
+        System.out.println("master上线版本1.0");
     }
 }
